dadf6514f7842c95f22800fad8f38116ef31bd78
sanket <sanketst@gmail.com>
2018-10-22 13:48:57 +0530

    Moving debugging statements to their own functions

--- tensorflow/core/grappler/optimizers/placement_optimizer.cc
+++ tensorflow/core/grappler/optimizers/placement_optimizer.cc
@@ -9,19 +9,15 @@
 namespace tensorflow {
 namespace grappler {
 
+#define MIN_EXECUTION_TIME 1000
+
 Status PlacementOptimizer::Optimize(Cluster* cluster, const GrapplerItem& item,
                                     GraphDef* optimized_graph) {
-  const DeviceSet* device_set = cluster->GetDeviceSet();
-  const std::vector<Device*>& devices = device_set->devices();
-
-  VLOG(0) << "Number of devices: " << devices.size();
-  for (int i = 0; i < devices.size(); i++) {
-    VLOG(0) << devices.at(i)->name()
-            << " 's attributes: " << devices.at(i)->DebugString() << "\n";
-  }
-
   VLOG(0) << "Optimize Grappler item: id=" << item.id;
+  // Output of the function
   *optimized_graph = item.graph;
+
+  PrintDeviceStats(cluster);
 
   AnalyticalCostEstimator estimator(cluster, true);
   Status initStatus = estimator.Initialize(item);
@@ -41,6 +37,25 @@ Status PlacementOptimizer::Optimize(Cluster* cluster, const GrapplerItem& item,
 
   VLOG(0) << "summary.execution_time: " << summary.execution_time << "\n";
 
+  if (summary.execution_time >= MIN_EXECUTION_TIME) {
+  }
+
+  // PrintCostStats(item, cost_graph);
+  return Status::OK();
+}
+
+void PrintDeviceStats(Cluster* cluster) {
+  const DeviceSet* device_set = cluster->GetDeviceSet();
+  const std::vector<Device*>& devices = device_set->devices();
+
+  VLOG(0) << "Number of devices: " << devices.size();
+  for (int i = 0; i < devices.size(); i++) {
+    VLOG(0) << devices.at(i)->name()
+            << " 's attributes: " << devices.at(i)->DebugString() << "\n";
+  }
+}
+
+void PrintCostStats(const GrapplerItem& item, CostGraphDef& cost_graph) {
   std::unordered_map<string, const CostGraphDef::Node*> name_to_cost;
 
   for (int i = 0; i < cost_graph.node_size(); i++) {
@@ -62,18 +77,16 @@ Status PlacementOptimizer::Optimize(Cluster* cluster, const GrapplerItem& item,
       cost_node = NULL;
     }
 
-    // if (cost_node) {
-    //  VLOG(0) << "Op: " << node.name()
-    //          << " max_memory_size: " << cost_node->max_memory_size()
-    //          << " memory_time: " << cost_node->memory_time()
-    //          << " compute_time: " << cost_node->compute_time()
-    //          << " compute_cost: " << cost_node->compute_cost() << "\n";
-    //} else {
-    //  VLOG(0) << "Op: " << node.name() << " has no cost estimate\n";
-    //}
+    if (cost_node) {
+      VLOG(0) << "Op: " << node.name()
+              << " max_memory_size: " << cost_node->max_memory_size()
+              << " memory_time: " << cost_node->memory_time()
+              << " compute_time: " << cost_node->compute_time()
+              << " compute_cost: " << cost_node->compute_cost() << "\n";
+    } else {
+      VLOG(0) << "Op: " << node.name() << " has no cost estimate\n";
+    }
   }
-
-  return Status::OK();
 }
 
 void PlacementOptimizer::Feedback(Cluster* cluster, const GrapplerItem& item,
