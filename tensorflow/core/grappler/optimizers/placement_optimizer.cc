0fd7f17e8c313c73b0bb5e1bb503e4446186e929
sanket <sanketst@gmail.com>
2018-10-22 13:59:42 +0530

    Fixing a compilation error

--- tensorflow/core/grappler/optimizers/placement_optimizer.cc
+++ tensorflow/core/grappler/optimizers/placement_optimizer.cc
@@ -37,7 +37,7 @@ Status PlacementOptimizer::Optimize(Cluster* cluster, const GrapplerItem& item,
 
   VLOG(0) << "summary.execution_time: " << summary.execution_time << "\n";
 
-  if (summary.execution_time >= MIN_EXECUTION_TIME) {
+  if (summary.execution_time >= Duration(MIN_EXECUTION_TIME)) {
   }
 
   // PrintCostStats(item, cost_graph);
